6cb15ee990ecff2eb2ac69a77737bfc54b47f948
Ishita <ishit726@gmail.com>
2026-07-05 13:02:33 +0530

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,1 +1,39 @@
+public class Calculator {
 
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) throws CalculatorException {
+        if (b == 0) {
+            throw new CalculatorException("Cannot divide by zero.");
+        }
+        return a / b;
+    }
+
+    public double modulus(double a, double b) throws CalculatorException {
+        if (b == 0) {
+            throw new CalculatorException("Cannot perform modulus by zero.");
+        }
+        return a % b;
+    }
+
+    public double power(double a, double b) {
+        return Math.pow(a, b);
+    }
+
+    public double squareRoot(double number) throws CalculatorException {
+        if (number < 0) {
+            throw new CalculatorException("Cannot calculate square root of a negative number.");
+        }
+        return Math.sqrt(number);
+    }
+}
